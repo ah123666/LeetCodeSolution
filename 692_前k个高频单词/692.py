af6f51d768fd815ea3a24b49371fb90d3178c5a0
xyym <1920376753@qq.com>
2021-03-11 21:32:15 +0800

    11

--- 692_前k个高频单词/692.py
+++ 692_前k个高频单词/692.py
@@ -1,3 +1,33 @@
+import heapq
+
+class Node:
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+
+    def __lt__(self, nxt):
+        return self.key > nxt.key if self.value == nxt.value\
+            else self.value < nxt.value
+
+
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
-           
+        mapping = {}
+        for word in words:
+            if word not in mapping:
+                mapping[word] = 0
+            mapping[word] = mapping[word] + 1
+
+        heap = []
+        for key, value in mapping.items():
+            heapq.heappush(heap, Node(key, value))
+            if len(heap) > k:
+                heapq.heappop(heap)
+
+        res = []
+        while len(heap) > 0:
+            temp = heapq.heappop(heap)
+            res.append(temp.key)
+        res.reverse()
+
+        return res

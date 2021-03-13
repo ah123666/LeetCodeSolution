2778ae1b89b247ae6c92463110b63263974c3909
xyym <1920376753@qq.com>
2021-03-13 10:55:56 +0800

    11

--- 1.两数之和/1.py
+++ 1.两数之和/1.py
@@ -1,16 +1,18 @@
 # 暴力法
 from typing import List
+
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-    	result = [0, 0]
-    	for i in range(0, len(nums)-1):
-    		for j in range(i, len(nums)):
-    			sum = nums[i] + nums[j]
-    			if sum == target:
-    				result[0] = i
-    				result[1] = j
-    				return result
-    	return result
+        result = [0, 0]
+        for i in range(0, len(nums) - 1):
+            for j in range(i, len(nums)):
+                sum = nums[i] + nums[j]
+                if sum == target:
+                    result[0] = i
+                    result[1] = j
+                    return result
+        return result
 
 
 s = Solution()
